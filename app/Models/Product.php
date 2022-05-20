66a860707746bd1cc9a3f4ef2c038a0954e0c98c
IvanSalandria <isalandria@gmail.com>
2022-05-20 17:42:26 -0300

    crud commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @property string $id;
+ * @property string $name;
+ * @property string $description;
+ * @property string $created_at;
+ * @property string $updated_at;
+ * @property string $deleted_at;
+ */
+
 
 class Product extends Model
 {
-    //
+    use SoftDeletes;
+    protected $keyType = 'integer';
+    
+    protected $fillable = [
+        'id',
+        'name',
+        'description',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
 }
